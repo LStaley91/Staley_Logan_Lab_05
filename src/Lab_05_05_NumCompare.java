dc60b6dcf9d74519f93bd31577b3247b749db270
LStaley <lstaley9185@gmail.com>
2022-09-22 15:15:42 -0400

    Task 5 (FINISHED)

---
+++ src/Lab_05_05_NumCompare.java
@@ -0,0 +1,20 @@
+public class Lab_05_05_NumCompare
+{
+    public static void main(String[] args)
+    {
+        double num1 = 12;
+        double num2 = 12.2;
+        if (num1 == num2)
+        {
+            System.out.println("Num1 and Num2 are the same.");
+        }
+        else if(num1>num2)
+        {
+        System.out.println("Num2 is less then and Num1.");
+        }
+        else
+        {
+            System.out.println("Num1 is less then and Num2.");
+        }
+    }
+}
